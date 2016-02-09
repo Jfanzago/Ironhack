44381eea2ce93bffb41d7c4492d4ad9c558abda4
jfanzago <jfanzago@gmail.com>
2016-02-09 11:42:02 -0500

    adding Emo Kylo Ren

--- week5/Day2/ajax/app.js
+++ week5/Day2/ajax/app.js
@@ -1,5 +1,20 @@
+
+$(document).on("ready", function(){
+	
+	$(".js-show-characters").on("click", function () {
+		fetchCharacters();
+	});
+
+	$(".js-add-kylo").on("click", function () {
+		publishKylo();
+	});
+});
+//----------------
+// ----Definitions--------
+function fetchCharacters () {
 $.ajax({
 	url: "https://ironhack-characters.herokuapp.com/characters",
+	
 	success: function(response){
 		console.log("Everything went okay");
 
@@ -9,7 +24,7 @@ $.ajax({
 		console.log("We screwed up!");
 	}
 });
-
+}
 
 function displayCharacters (characters) {
 	characters.forEach(function (char){
@@ -23,4 +38,41 @@ function displayCharacters (characters) {
 		`;
 		$(".js-characters-list").append(html);
 	});
+}
+
+function publishKylo (){
+   var newCharacter = {
+   	name: "Emo Kylo Ren",
+   	occupation: "whiner",
+   	weapon:"emo"
+   };
+
+
+
+$.ajax({
+	type: "POST",
+
+	url: "https://ironhack-characters.herokuapp.com/characters",
+
+	data: newCharacter,
+
+	success: function (response) {
+		var html = `
+		<li>
+		<h2> ${newCharacter.name} </h2>
+		<p># ${response.id} </p>
+		<p>Occupation: ${newCharacter.occupation}</p>
+		<p>Weapon: ${newCharacter.weapon}</p>
+		</li>
+		`;
+		$(".js-characters-list").append(html);
+
+		alert("Emo Kylo Ren added successfully.");
+
+	},
+	
+	error: function () {
+		alert("Could not add Kylo Ren.");
+	}
+});
 }
