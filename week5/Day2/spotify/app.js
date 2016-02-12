03cb36af8388d67c5fd58fb5732b4ce54d11e303
jfanzago <jfanzago@gmail.com>
2016-02-11 21:19:53 -0500

    initial geo commit

--- week5/Day2/spotify/app.js
+++ week5/Day2/spotify/app.js
@@ -1,28 +1,49 @@
-  $(document).on("ready", function(){
-    console.log('aaaaaaaaaa')
+$(document).on("ready", function(){
+  $(".js-artist-submit").on("click", getSpotifyArtists)
+})
 
-    $('.js-submit').on('click', onSubmit);
-
-});
-
-//-------- Definitions ---------
-
-function onSubmit(e) {
-  console.log('bbbbbbb')
-  console.log(e);
-  e.preventDefault();
-
-var artistsearch = $("#artist").val();
+function getSpotifyArtists(){
+  var searchTerm = $('.js-artist-input')
+                      .val()
+                      .split(" ")
+                      .join("+");
 
   $.ajax({
-    url: "https://api.spotify.com/v1/search?type=artist&query="+artistsearch,
-    success: function (response) {
-      console.log(response.artists.items);
-      // alert("SUBMITTTEDD");
-    },
-    error: function () {
-      alert("!!!!!Uh-Oh whoopsies!!!!!!");
-    }
+    url: "https://api.spotify.com/v1/search?type=artist&query=" + searchTerm,
+    success: onArtistFindSuccess, 
+    error: onArtistFindError
+  })
+  // This clears the input box's term
+  $('.js-artist-input').val("")
+}
 
-  });
+function onArtistFindSuccess(artistResponse){
+  $(".artist-list").empty();
+
+  artistResponse.artists.items.forEach(function(artist){
+    createArtistHtml(artist);
+  })
+}
+
+function createArtistHtml(artist){
+  var image;
+
+  if (artist.images.length > 0){
+    image = artist.images[0].url
+  } else {
+    image = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCk2bayZHUJsWeglTeTOvjcX3PvSpnkqU3T-6YmCE6bT1nFQ56Bw"
+  }
+
+  var html = `
+  <li class="artist-item">
+    <h4>${artist.name}</h4>
+    <img class="artist-image" src="${image}">
+  </li>
+  `;
+
+  $(".artist-list").append(html);
+}
+
+function onArtistFindError(err){
+  console.log("You dun goofed", err);
 }
--- week5/Day3/geolocation/app.js
+++ week5/Day3/geolocation/app.js
@@ -1,20 +1,34 @@
-if ("geolocation" in naviagtor) {
- 
- getUserlocation();
+if ("geolocation" in navigator) {
+  // Browser has geolocation
+  getUserLocation();
 }
+
 else {
-
+  // Browser does NOT have geolocation
 }
 
-//-------------------
 
-function getUserlocation (){
-	naviagtor.geolocation.getCurrentPosition( successCallback, errorCallback,);
+
+// ===============================================
+// --------------  DEFINITIONS  ------------------
+
+function getUserLocation () {
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  // optional 3rd parameter: optionsObj
 }
 
-function sucessCallback (position){
-	console.log("SUCCESS!!");
-	console.log(position);
-	console.log("Lat", position.coords.latitude);
-	console.log("Long", position.coords.longitude);
+
+function successCallback (position) {
+  console.log("SUCCESS!!");
+  console.log(position);
+  console.log("Lat", position.coords.latitude);
+  console.log("Long", position.coords.longitude);
+
+
+	$(".js-lat-value").text(position.coords.latitude);
+	$(".js-lng-value").text(position.coords.longitude);
+}
+
+function errorCallback (error) {
+  console.log("ERROR", error);
 }
